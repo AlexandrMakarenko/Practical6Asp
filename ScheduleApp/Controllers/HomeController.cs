c58a45d9801ebb6394dd98675fb678fc841d394c
AlexandrMakarenko <sahka.makar4ik.gta@gmail.com>
2020-03-20 22:49:21 +0300

    Initial commit

---
+++ ScheduleApp/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using ScheduleApp.Models;
+
+namespace ScheduleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // изменить на путь к App_Data, где скачан файл
+        private const string BasePath =
+            @"D:\Рабочий стол\Практические\Practical6Asp\ScheduleApp\App_Data\";
+
+        public ActionResult СhangeWeek(string type)
+        {
+            var config = new Config() { ThisWeek = type};
+            using (var sw = new StreamWriter(BasePath + "Config.json"))
+            {
+                sw.Write(JsonConvert.SerializeObject(config));
+            }
+            return RedirectToAction("Index");
+        }
+
+        // вызов смены недели
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Вся недели"; // подпись вкладки
+            ViewBag.Json = GetSchedule();
+            return View();
+        }
+
+        // выбор дня, ДЛЯ ПОКАЗА в зависимости от недели меняется расписание
+        public ActionResult Schedule(int idDay)
+        {
+            switch (idDay)
+            {
+                case 1:
+                    ViewBag.Json = GetDay("Первый день");
+                    break;
+                case 2:
+                    ViewBag.Json = GetDay("Второй день");
+                    break;
+                case 3:
+                    ViewBag.Json = GetDay("Третий день");
+                    break;
+                case 4:
+                    ViewBag.Json = GetDay("Четвертый день");
+                    break;
+                case 5:
+                    ViewBag.Json = GetDay("Пятый день");
+                    break;
+                case 6:
+                    ViewBag.Json = GetDay("Нулевой день");
+                    break;
+                default:
+                    return Redirect("https://localhost:44320/"); 
+            }
+            return View();
+        }
+
+        //смена недели
+        private Schedule GetSchedule()
+        {
+            using (var sr = new StreamReader(PathToData()))
+            {
+                return JsonConvert.DeserializeObject<Schedule>(sr.ReadToEnd());
+            }
+        }
+
+        // считывание из json файла инфы за определенный день
+        private Day GetDay(string dayOfWeek)
+        {
+            using (var sr = new StreamReader(PathToData()))
+            {
+                return JsonConvert.DeserializeObject<Schedule>(sr.ReadToEnd()).Days.First(x => x.Type == dayOfWeek);
+            }
+        }
+
+        // смена недели, по дефолту стоит верхняя
+        private string PathToData()
+        {
+            using (var sr = new StreamReader(BasePath + "Config.json"))
+            {
+                var thisWeek = JsonConvert.DeserializeObject<Config>(sr.ReadToEnd()).ThisWeek;
+                switch (thisWeek)
+                {
+                    case "Lower":
+                        return BasePath + "LowerSchedule.json";
+                    case "Upper":
+                        return BasePath + "UpperSchedule.json";
+                    default:
+                        return "UpperSchedule.json";
+                }
+            }
+        }
+    }
+}
---
+++ ScheduleApp/Models/Schedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScheduleApp.Models
+{
+    public class Schedule
+    {
+        public List<Day> Days { get; set; }
+    }
+
+    public class Day
+    {
+        public string Type { get; set; }
+        public List<Lesson> Lessons { get; set; }
+    }
+
+    public class Lesson
+    {
+        public string Title { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Classroom { get; set; }
+        public string Teacher { get; set; }
+    }
+}
